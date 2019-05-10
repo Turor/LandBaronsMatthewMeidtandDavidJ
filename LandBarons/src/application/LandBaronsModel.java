bebd6fda6b5a7812f0454fcd4d8cfdeab72f61f7
Turor <dragosduislidur@gmail.com>
2019-05-10 15:07:06 -0500

    Finished non-Dijkstra initialization methods

--- LandBarons/src/application/LandBaronsModel.java
+++ LandBarons/src/application/LandBaronsModel.java
@@ -1,36 +1,42 @@
 package application;
 
-import java.util.LinkedList;
+import java.util.Objects;
+import java.util.Random;
 
 public class LandBaronsModel {
 	
+	/**
+	 * Size adds an extra 2 cells to the vertical and horizontal
+	 * size to facilitate edge detection without generation of a
+	 * stack trace
+	 */
 	private int size;
-	
-	private int turnCount;
-	
-	private boolean winnable;
-	
-	private LandNode[][] board;
-	
-	private int playerOneBudget;
-	
-	private int playerTwoBudget;
-	
-	private boolean passedLastTurn;
-	
-	private boolean gameFinished;
-	
 
-	
+	private int turnCount;
+
+	private boolean winnable;
+
+	private LandNode[][] board;
+
+	private int playerOneBudget;
+
+	private int playerTwoBudget;
+
+	private boolean passedLastTurn;
+
+	private boolean gameFinished;
+
+
+
 	public LandBaronsModel(int size) {
-		initializeModel(size);
+		initializeModel(size+2);
 	}
-	
+
 	public void changeSize(int size) {
 		//TODO: Maybe warn players of their actions?	
-		initializeModel(size);
+		initializeModel(size+2);
 	}
-	
+
 	public void makeMove(int row, int col) {
 		if(isValidMove(row, col))
 			//TODO: do the move
@@ -41,27 +47,27 @@ public class LandBaronsModel {
 			//TODO: Inform the view why the move was illegal
 			;
 	}
-	
+
 	public void pass() {
 		if(passedLastTurn)
 			gameFinished();
 		else
 			passedLastTurn = true;
 	}
-	
+
 	public void reset() {
 		winnable = false;
 		turnCount = 0;
 		initBudget();
-		
+
 		while(!winnable) {
 			resetNodes();
 			addSpecialNodes();
 			testWinnability();
 		}
-		
+
 	}
-	
+
 	private void initializeModel(int size) {
 		this.size = size;
 		turnCount = 0;
@@ -72,138 +78,119 @@ public class LandBaronsModel {
 		passedLastTurn = false;
 		gameFinished = false;
 	}
-	
+
 	private void initializeBoard() {
 		constructBoard();
+		addSpecialNodes();
 		connectBoard();
 	}
-	
+
 	private void constructBoard() {
-		for(int row = 0; row < board.length; row++) {
-			for(int col = 0; col < board[row].length;col++) {
+		for(int row = 1; row < board.length-1; row++) {
+			for(int col = 1; col < board[row].length-1;col++) {
 				board[row][col] = new LandNode();
 			}
 		}
-		
+		board[1][1].setOwnership(SOURCE_OR_DESTINATION);
+		board[size-2][size-2].setOwnership(SOURCE_OR_DESTINATION);
+
 	}
-	
+
 	private void connectBoard() {
-		makeInternalNodeConnections();
-		makeFirstRowConnections();
-		makeLastRowConnections();
-		makeFirstColumnConnections();
-		makeLastColumnConnections();
-		makeCornerConnections();
-	}
-	
-	private void makeInternalNodeConnections() {
-		for(int row = 1; row < board.length-1;row++)
+		for(int row = 1; row < board.length -1; row++) {
 			for(int col = 1; col < board[row].length-1;col++) {
-				connectABOVE(row,col);
-				connectRIGHT_OF(row,col);
-				connectBELOW(row,col);
-				connectLEFT_OF(row,col);
+				if(null != board[row-1][col] ) {
+					board[row][col].connectNodes(board[row-1][col],ABOVE);
+				}if( null != board[row][col+1]) {
+					board[row][col].connectNodes(board[row][col+1],RIGHT_OF);
+				}if( null != board[row+1][col]) {
+					board[row][col].connectNodes(board[row+1][col],BELOW);
+				}if( null != board[row][col-1]) {
+					board[row][col].connectNodes(board[row][col-1],LEFT_OF);
+				}
 			}
-	}
-	
-	private void connectABOVE(int row, int col) { 
-		board[row][col].connectNodes(board[row-1][col],ABOVE);
-	}
-	
-	private void connectRIGHT_OF(int row, int col) {
-		board[row][col].connectNodes(board[row][col+1],RIGHT_OF);
-	}
-	
-	private void connectBELOW(int row, int col) {
-		board[row][col].connectNodes(board[row+1][col],BELOW);
-	}
-	
-	private void connectLEFT_OF(int row, int col) {
-		board[row][col].connectNodes(board[row][col-1],LEFT_OF);
-	}
-
-	private void makeFirstRowConnections() {
-		for(int col = 1,row = 0; col < board.length-1;col++) {
-			connectRIGHT_OF(row,col);
-			connectBELOW(row,col);
-			connectLEFT_OF(row,col);
 		}
-	}
-
-	private void makeLastRowConnections() {
-		for(int col = 1, row = board.length-1; col < board.length-1; col++) {
-			connectABOVE(row,col);
-			connectRIGHT_OF(row,col);
-			connectLEFT_OF(row,col);
-		}
-	}
-	
-	private void makeFirstColumnConnections() {
-		for(int col = 0, row = 1; row < board.length-1; row++) {
-			connectABOVE(row,col);
-			connectRIGHT_OF(row,col);
-			connectBELOW(row,col);
-		}
-	}
-	
-	private void makeLastColumnConnections() {
-		for(int col = board.length-1, row = 1; row < board.length-1; row++) {
-			connectABOVE(row,col);
-			connectLEFT_OF(row,col);
-			connectBELOW(row,col);
-		}
-	}
-	
-	private void makeCornerConnections() {
-		connectBELOW(0,0);
-		connectRIGHT_OF(0,0);
-		connectLEFT_OF(0,board.length-1);
-		connectBELOW(0,board.length-1);
-		connectABOVE(board.length-1,0);
-		connectRIGHT_OF(board.length-1,0);
-		connectABOVE(board.length-1,board.length-1);
-		connectLEFT_OF(board.length-1,board.length-1);
 	}
 	
 	private void initBudget() {
-		playerOneBudget = size*size*2;
+		playerOneBudget = (size-2)*(size-2)*2;
 		playerTwoBudget = playerOneBudget;
 	}
-	
+
 	private void gameFinished() {
 		//TODO: for each node in path, sum player one and player 2
 		//TODO: Add unspent budgets to their players sums*10
 		//TODO: Inform listeners that the game is done and disallow further moves game moves
 	}
-	
+
 
 	private void resetNodes() {
-		for(int row = 0; row < board.length;row++)
-			for(int col = 0; col < board[row].length;col++)
+		for(int row = 1; row < board.length-1;row++)
+			for(int col = 1; col < board[row].length-1;col++)
 				board[row][col].reset();
 	}
 	
+	//1 through size - 2
 	private void addSpecialNodes() {
-		//TODO: Use random number generator to add the special nodes
-		// 			- Ensure proper collision behavior
+		Random yolo = new Random();
+		for(int neededSpecialTiles = 0; neededSpecialTiles < size-2;neededSpecialTiles++) {
+			boolean failedToAddTile = true;
+			while(failedToAddTile) {
+				int row = yolo.nextInt(size-2) + 1;
+				int col = yolo.nextInt(size-2) + 1;
+				if(isValidTileLocation(row, col)) 
+					if(noCollision(row,col)) 
+						if(yolo.nextInt(2) == 0) {
+							board[row][col].setOwnership(PUBLIC_LAND);
+							failedToAddTile = false;
+						}else {
+							board[row][col].setOwnership(COMPANY_OWNED);
+							failedToAddTile = false;
+						}
+				
+			}
+		}
 	}
-	
+
+	/**
+	 * A random tile can initially be placed at any tile besides the start and ending tile
+	 * for the companies.
+	 * @param horizontal
+	 * @param vertical
+	 * @return A potentially valid location was found
+	 */
+	private boolean isValidTileLocation(int horizontal, int vertical) {
+		if(horizontal == 1 && vertical == 1)
+			return false;
+		else if(horizontal == size - 2 && vertical == size - 2)
+			return false;
+		else
+			return true;
+	}
+
+	private boolean noCollision(int row, int col) {
+		if(board[row][col].getOwnership() < 0)
+			return false;
+		else
+			return true;
+	}
+
 	private void testWinnability() {
 		//TODO: Use cheapestPaths result to determine that a path exists
 		//TODO: Change winnable accordingly
 	}
-	
+
 	private void cheapestPath(){
-		
+
 		//TODO: Write Djikstras
 
 	}
-	
+
 	private boolean isValidMove(int row, int col) {
 		//TODO: Determine if the move is valid
 		return false;
 	}
-	
+
 	public String toString() {
 		//TODO: Inform users what the status of the game is
 		String s = "";
@@ -220,21 +207,20 @@ public class LandBaronsModel {
 		return s;
 	}
 	
-	
-	
-	
+	/**
+	 * @return - Copy of the game board, useful for debugging
+	 */
+	public LandNode[][] getBoardClone(){
+		return board.clone();
+	}
 
-	
-	
-	
-	
-	
+	private static final int SOURCE_OR_DESTINATION = -3;
 	private static final int PUBLIC_LAND = -2;
 	private static final int COMPANY_OWNED = -1;
 	private static final int UNOWNED = 0;
 	private static final int PLAYER_ONE_OWNED = 1;
 	private static final int PLAYER_TWO_OWNED = 2;
-	
+
 	private static final int ABOVE = 0;
 	private static final int RIGHT_OF = 1;
 	private static final int BELOW = 2;
